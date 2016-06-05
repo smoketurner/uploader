127566ea4ce28f5bd4f936bfed4ced9f6d57326e
Justin Plock <jplock@smoketurner.com>
2016-06-05 11:11:10 -0400

    Added a benchmark for generating an S3 key

---
+++ uploader-application/src/test/java/com/smoketurner/uploader/application/benchmarks/UploaderBenchmark.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2016 Smoke Turner, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.smoketurner.uploader.application.benchmarks;
+
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import com.smoketurner.uploader.application.config.AwsConfiguration;
+import com.smoketurner.uploader.application.core.Uploader;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class UploaderBenchmark {
+
+    private final Uploader uploader = new Uploader(new AwsConfiguration());
+
+    @Benchmark
+    public String generateKey() {
+        return uploader.generateKey();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Runner(new OptionsBuilder()
+                .include(UploaderBenchmark.class.getSimpleName()).forks(1)
+                .warmupIterations(5).measurementIterations(5).build()).run();
+    }
+}
