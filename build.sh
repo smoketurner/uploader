cc1964d0f50bf09d16d726c792cb2060dc8cb746
Justin Plock <jplock@smoketurner.com>
2016-10-22 20:09:18 -0400

    Clean up build script

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@
 
 VERSION="1.0.0-SNAPSHOT"
 
-docker rm -f build-cont
-docker build -t build-img -f Dockerfile.build .
-docker create --name build-cont build-img
-docker cp "build-cont:/src/uploader-application/target/uploader-application-${VERSION}.jar" ./uploader.jar
 docker build --build-arg VERSION=${VERSION} -t "smoketurner/uploader:${VERSION}" .
