84ab6ea5410879929a342cbbecf3e52de2bc176e
Justin Plock <jplock@smoketurner.com>
2017-02-18 10:47:45 -0500

    Clean up docker process

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 VERSION=`xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml`
 
-docker run --rm=true -p 4433:4433 -p 8443:8443 smoketurner/uploader:${VERSION}
+docker run \
+--name uploader \
+--rm \
+-e PORT=4433 \
+-p 4433:4433 \
+-p 8443:8443 \
+smoketurner/uploader:${VERSION}
